9c0001e4d9f8bdbe335cdb16e6470364acb8d5b7
Peter Vijeh <pvijeh@gmail.com>
2015-12-16 16:40:35 -0500

    working

--- common/models/vote.js
+++ common/models/vote.js
@@ -1,3 +1,61 @@
+
+
 module.exports = function(Vote) {
+
+    var getVoteCats = function (callback){
+        Vote.app.models.VoteCategory.find({
+        order: 'Id ASC' }, function(err, attributes) {
+          callback(attributes);
+      }); 
+    }
+
+    var getPlace = function (callback){
+        Vote.app.models.Place.find({
+        where: {Id : 1}},
+         function(err, attributes) {
+          callback(attributes);
+      }); 
+    }
+
+
+  Vote.getPlaceVotesbyCategory = function(id, cb ) {
+
+    Vote.find({where : {PlaceId : 1 }}, function(err, placeVotes) {
+      if (err) return cb(err);
+
+
+
+
+
+      var AddResultsFromVoteCats = function (attributes) {
+
+      var dataPackage = {
+        placeAttributes : attributes, 
+        votes : placeVotes 
+      }
+
+        cb(null, dataPackage);  
+      }
+
+      getVoteCats(AddResultsFromVoteCats); 
+
+      // var len = placeVotes.length; 
+      // var countVoteCategories = {}; 
+
+      // for (var i = 0, len; i < len; i++){
+      //   var categoryNumber = placeVotes[i].VoteCatId; 
+ 
+      // }
+    
+    });
+  };
+
+  Vote.remoteMethod('getPlaceVotesbyCategory', {
+    accepts: [
+      {arg: 'id', type: 'number'},
+    ],
+    returns: {arg: 'success', type: 'boolean'},
+    http: {path:'/getplacevotesbycategory', verb: 'get'}
+  });
 
 };
--- server/boot/createUser.js
+++ server/boot/createUser.js
@@ -2,43 +2,50 @@ module.exports = function(app) {
   var user = app.models.AppUser;
   var vote = app.models.Vote;
   var place = app.models.Place;
+  var voteCat = app.models.VoteCategory;
 
   var today = new Date();
   var now = today.getDate();
 
-  user.destroyAll(); 
-  vote.destroyAll(); 
-  place.destroyAll(); 
 
-    var make_users = [], 
-    make_places = [],
-    make_votes = [],
-    make_voteCats = []; 
+//
+// clear data 
+//
+
+  // user.destroyAll(); 
+  // vote.destroyAll(); 
+  // place.destroyAll(); 
+  // voteCat.destroyAll(); 
+
+    // var make_users = [], 
+    // make_places = [],
+    // make_votes = [],
+    // make_voteCats = []; 
 
     //
     // generates some user data 
     //
 
-    var generateUsers = function (i, max){
-            while ( i < max)  {
-            make_randomString =  Math.random().toString(36).substring(7);
+        // var generateUsers = function (i, max){
+        //         while ( i < max)  {
+        //         make_randomString =  Math.random().toString(36).substring(7);
 
-            make_users.push({
-                id  :  i,
-                username :  make_randomString,
-                email : make_randomString+'@email.com',
-                password :  'hovergear'
-            }); 
-            ++i; 
-        }    
-    }
+        //         make_users.push({
+        //             id  :  i,
+        //             username :  make_randomString,
+        //             email : make_randomString+'@email.com',
+        //             password :  'hovergear'
+        //         }); 
+        //         ++i; 
+        //     }    
+        // }
 
-    generateUsers(0, 100); 
+        // generateUsers(0, 100); 
 
-    user.create(make_users, function(err, users) {
-      if (err) throw err;
-      console.log('Created users:', users);
-    });
+        // user.create(make_users, function(err, users) {
+        //   if (err) throw err;
+        //   console.log('Created users:', users);
+        // });
 
     //////////////////////////////// end generate users  ////////////////////////////////////////
 
@@ -46,27 +53,27 @@ module.exports = function(app) {
 //  generates some place data 
 //
 
-    var generatePlaces = function (i, max){
-            while ( i < max)  {
+        // var generatePlaces = function (i, max){
+        //         while ( i < max)  {
 
-            make_places.push({
-                Id  :  i,
-                Category :  'category',
-                Address : 'address', 
-                GooglePlacesId :  12345918181,
-                Name : make_randomString =  'venue_'+i,
-                Neighborhood : 'neighborhood'
-            }); 
-            ++i; 
-        }    
-    }
-    
-    generatePlaces(0, 50); 
+        //         make_places.push({
+        //             Id  :  i,
+        //             Category :  'category',
+        //             Address : 'address', 
+        //             GooglePlacesId :  12345918181,
+        //             Name : make_randomString =  'venue_'+i,
+        //             Neighborhood : 'neighborhood'
+        //         }); 
+        //         ++i; 
+        //     }    
+        // }
+        
+        // generatePlaces(0, 50); 
 
-    place.create(make_places, function(err, places) {
-      if (err) throw err;
-      console.log('Created places:', places);
-    });
+        // place.create(make_places, function(err, places) {
+        //   if (err) throw err;
+        //   console.log('Created places:', places);
+        // });
   
 //////////////////////////////// end generate places ////////////////////////////////////////
 
@@ -75,34 +82,58 @@ module.exports = function(app) {
 //  generate vote data 
 //
 
-       var generateVotes = function (i, max){
-            var today = new Date();
+        //    var generateVotes = function (i, max){
+        //         var today = new Date();
 
-            while ( i < max)  {
+        //         while ( i < max)  {
 
-            make_votes.push({
-                Id  :  i,
-                placeId: Math.floor(Math.random() * (49 - 0)),
-                UserId : Math.floor(Math.random() * (49 - 0)),
-                VoteCatId: Math.floor(Math.random() * (19 - 0)),
-                votedFor:  true,
-                DateAdded : today.getDate(),
-                DateModified : today.getDate()
-            }); 
-            ++i; 
-        }    
-    }
+        //         make_votes.push({
+        //             Id  :  i,
+        //             PlaceId: Math.floor(Math.random() * (49 - 0)),
+        //             UserId : Math.floor(Math.random() * (49 - 0)),
+        //             VoteCatId: Math.floor(Math.random() * (19 - 0)),
+        //             votedFor:  true,
+        //             DateAdded : today.getDate(),
+        //             DateModified : today.getDate()
+        //         }); 
+        //         ++i; 
+        //     }    
+        // }
 
-    generateVotes(0, 100); 
+        // generateVotes(0, 1000); 
 
-    vote.create(make_votes, function(err, votes) {
-      if (err) throw err;
-      console.log('Created votes:', votes);
-    });
+        // vote.create(make_votes, function(err, votes) {
+        //   if (err) throw err;
+        //   console.log('Created votes:', votes);
+        // });
   
 //////////////////////////////// end generate places ////////////////////////////////////////
 
 
+// 
+//  generate vote category data 
+//
+
+        //    var generateVoteCat = function (i, max){
+
+        //         while ( i < max)  {
+
+        //         make_voteCats.push({
+        //             Id  :  i,
+        //             AttributeName : 'attribute'+ i
+        //         }); 
+        //         ++i; 
+        //     }    
+        // }
+
+        // generateVoteCat(0, 50); 
+
+        // voteCat.create(make_voteCats, function(err, voteCats) {
+        //   if (err) throw err;
+        //   console.log('Created votes:', voteCats);
+        // });
+  
+//////////////////////////////// end generate places ////////////////////////////////////////
 
 
 

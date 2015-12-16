b4924f7c6a5d75ee2c93d57912257c35931a7c89
Peter Vijeh <pvijeh@gmail.com>
2015-12-16 17:28:02 -0500

    adding place

--- common/models/vote.js
+++ common/models/vote.js
@@ -2,6 +2,7 @@
 
 module.exports = function(Vote) {
 
+    // get all voteCategory model data and sort it 
     var getVoteCats = function (callback){
         Vote.app.models.VoteCategory.find({
         order: 'Id ASC' }, function(err, attributes) {
@@ -9,22 +10,26 @@ module.exports = function(Vote) {
       }); 
     }
 
-    var getPlace = function (callback){
+    // get place model data for target id
+    var getPlace = function (id, dataPackage, callback){
         Vote.app.models.Place.find({
-        where: {Id : 1}},
-         function(err, attributes) {
-          callback(attributes);
+        where: {Id : id}},
+         function(err, place) {
+          callback(place, dataPackage); 
       }); 
     }
 
-
   Vote.getPlaceVotesbyCategory = function(id, cb ) {
 
-    Vote.find({where : {PlaceId : 1 }}, function(err, placeVotes) {
+    if (typeof id === 'undefined') {
       if (err) return cb(err);
+      
+      cb(null, 'please include id parameter in the form of a number');  
 
+    } else {
 
-
+    Vote.find({where : {PlaceId : id }}, function(err, placeVotes) {
+      if (err) return cb(err);
 
 
       var AddResultsFromVoteCats = function (attributes) {
@@ -34,20 +39,23 @@ module.exports = function(Vote) {
         votes : placeVotes 
       }
 
-        cb(null, dataPackage);  
+        // call get place -- then add place to datapackage then send datapackage to api route. 
+        getPlace(id, dataPackage, addResultsFromPlace); 
       }
 
+      var addResultsFromPlace = function (place, dataPackage) {
+
+        dataPackage.place = place; 
+
+        cb(null, dataPackage);  
+      } 
+
+      // call get vote cats, then use callback to add results to dataPackage
       getVoteCats(AddResultsFromVoteCats); 
-
-      // var len = placeVotes.length; 
-      // var countVoteCategories = {}; 
-
-      // for (var i = 0, len; i < len; i++){
-      //   var categoryNumber = placeVotes[i].VoteCatId; 
- 
-      // }
     
     });
+
+    }
   };
 
   Vote.remoteMethod('getPlaceVotesbyCategory', {

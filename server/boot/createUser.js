d05d4c95d4283566e6696fc4e106dcd335105f6d
peter vijeh <peter@peters-MacBook-Pro.local>
2016-01-01 19:44:19 -0500

    f

--- server/boot/createUser.js
+++ server/boot/createUser.js
@@ -92,13 +92,13 @@ module.exports = function(app) {
 
   // user.destroyAll(); 
   // vote.destroyAll(); 
-  place.destroyAll(); 
-  voteCat.destroyAll(); 
+  // place.destroyAll(); 
+  // voteCat.destroyAll(); 
 
     // var make_users = []; 
     // var make_votes = [];
-    var make_places = [];
-    var make_voteCats = []; 
+    // var make_places = [];
+    // var make_voteCats = []; 
 
     //
     // generates some user data 
@@ -131,30 +131,30 @@ module.exports = function(app) {
 //  generates some place data 
 //
 
-        var generatePlaces = function (i, max){
-                while ( i < max)  {
+        // var generatePlaces = function (i, max){
+        //         while ( i < max)  {
 
-                make_places.push({
-                    Id  :  i,
-                    Category :  'category',
-                    Address : 'address', 
-                    GooglePlacesId :  12345918181,
-                    Name : make_randomString =  'venue_'+i,
-                    Neighborhood : 'neighborhood',
-                    Address : '123 Fake Street',
-                    Route : 'venue_'+i,
-                    VenueType : 'Nightclub' 
-                }); 
-                ++i; 
-            }    
-        }
+        //         make_places.push({
+        //             Id  :  i,
+        //             Category :  'category',
+        //             Address : 'address', 
+        //             GooglePlacesId :  12345918181,
+        //             Name : make_randomString =  'venue_'+i,
+        //             Neighborhood : 'neighborhood',
+        //             Address : '123 Fake Street',
+        //             Route : 'venue_'+i,
+        //             VenueType : 'Nightclub' 
+        //         }); 
+        //         ++i; 
+        //     }    
+        // }
         
-        generatePlaces(0, 50); 
+        // generatePlaces(0, 50); 
 
-        place.create(make_places, function(err, places) {
-          if (err) throw err;
-          console.log('Created places:', places);
-        });
+        // place.create(make_places, function(err, places) {
+        //   if (err) throw err;
+        //   console.log('Created places:', places);
+        // });
   
 //////////////////////////////// end generate places ////////////////////////////////////////
 
@@ -194,32 +194,32 @@ module.exports = function(app) {
 //  generate vote category data 
 //
 
-   var generateVoteCat = function (i, max){
+   // var generateVoteCat = function (i, max){
         
-        while ( i < max)  {
+   //      while ( i < max)  {
 
-        make_voteCats.push({
-            Id  :  i,
-            AttributeName : categories[i],
-            Description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui.',
-            Route: 'category-route-'+[i],
-            CategoryIcon: 'fa fa-anchor',
-            Neighborhood: 'Chelsea', 
-            VenueType: 'Nightclub', 
-            PageTitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus.'
-        }); 
+   //      make_voteCats.push({
+   //          Id  :  i,
+   //          AttributeName : categories[i],
+   //          Description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui.',
+   //          Route: 'category-route-'+[i],
+   //          CategoryIcon: 'fa fa-anchor',
+   //          Neighborhood: 'Chelsea', 
+   //          VenueType: 'Nightclub', 
+   //          PageTitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus.'
+   //      }); 
         
-        ++i; 
+   //      ++i; 
         
-        }    
-    }
+   //      }    
+   //  }
 
-    generateVoteCat(0, categories.length); 
+   //  generateVoteCat(0, categories.length); 
 
-    voteCat.create(make_voteCats, function(err, voteCats) {
-      if (err) throw err;
-      console.log('Created votes:', voteCats);
-    });
+   //  voteCat.create(make_voteCats, function(err, voteCats) {
+   //    if (err) throw err;
+   //    console.log('Created votes:', voteCats);
+   //  });
   
 //////////////////////////////// end generate places ////////////////////////////////////////
 
